123a6f5fa1c2642bf3600dd553db1fabee0b0938
jahongir136 <rasuljonov_96@naver.com>
2023-01-26 12:57:51 +0900

    feat: develop signup business logic

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,13 +1,18 @@
+const Member = require("../models/Member");
+
 let memberController = module.exports;
 
-memberController.home = (req, res) => {
-  console.log("GET cont.home");
-  res.send("home sahifadasiz");
-};
+memberController.signup = async (req, res) => {
+  try {
+    console.log("POST: cont/signup");
+    const data = req.body;
+    const member = new Member();
+    const new_member = await member.signupData(data);
 
-memberController.signup = (req, res) => {
-  console.log("POST cont.signup");
-  res.send("signup sahifadasiz");
+    res.send("done");
+  } catch (err) {
+    console.log("ERROR, cont/signup");
+  }
 };
 
 memberController.login = (req, res) => {
