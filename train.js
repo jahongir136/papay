b2e22044a8011741ed033f7d644d56b5f69ad504
jahongir136 <rasuljonov_96@naver.com>
2023-01-27 00:08:19 +0900

    member login prosess development

--- train.js
+++ train.js
@@ -1,6 +1,6 @@
 // K-TASK
 
-function FootBallPoints(wins, draws, losses) {
-  return wins * 3 + draws * 1 + losses * 0;
-}
-console.log(FootBallPoints(3, 4, 2));
+// function FootBallPoints(wins, draws, losses) {
+//   return wins * 3 + draws * 1 + losses * 0;
+// }
+// console.log(FootBallPoints(3, 4, 2));
