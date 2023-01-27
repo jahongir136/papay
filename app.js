dd326576eb3361bc09097bd30bbefc5075632096
jahongir136 <rasuljonov_96@naver.com>
2023-01-27 23:35:27 +0900

    feat: bcrypt package asosida passwordni hash qlish

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+console.log("wev server boshlash");
 const express = require("express");
 const app = express();
-const router = require("./router");
+const router = require("./router.js");
+// const router_bssr = require("./router_bssr.js");
+
+// let session = require("express-session");
+// const MongoDBStore = require("connect-mongodb-session")(session);
+// const store = new MongoDBStore({
+//     uri: process.env.MONGO_URL,
+//     collection: "sessions",
+// });
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -8,7 +17,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2 session code
-
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {
+//         maxAge: 1000 * 60 * 30, // for 30 minutes
+//     }
+// }))
 // 3 views code
 app.set("views", "views");
 app.set("view engine", "ejs");
---
+++ router_bssr.js
@@ -0,0 +1,17 @@
+// const express = require("express");
+// const router_bssr = express.Router();
+// const restaurantController = require("./controllers/restaurantController");
+
+// //*******************************************
+// /*               BSSR EJS           *
+// /**************************************** */
+
+// router_bssr.get("/signup", restaurantController.signup);
+// router_bssr.post("/signup", restaurantController.signup);
+
+// router_bssr.get("/login", restaurantController.logout);
+// router_bssr.post("/login", restaurantController.login);
+
+// router_bssr.get("/logout", restaurantController.logout);
+
+// module.exports = router_bssr;
