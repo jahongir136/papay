fd5c40f8e2cbe698383c709029ba14701acb09d4
jahongir136 <rasuljonov_96@naver.com>
2023-03-18 12:31:48 +0900

    feat: develop getArticles based on targets api business logic

--- models/Community.js
+++ models/Community.js
@@ -4,6 +4,7 @@ const Definer = require("../lib/mistake");
 const {
   shapeIntoMongooseObjectId,
   board_id_enum_list,
+  board_article_status_enum_list,
 } = require("../lib/config");
 
 class Community {
@@ -34,7 +35,7 @@ class Community {
 
   async getMemberArticlesData(member, mb_id, inquery) {
     try {
-      const auth_mb_id = shapeIntoMongooseObjectId(member?.mb_id);
+      const auth_mb_id = shapeIntoMongooseObjectId(member?._id);
       mb_id = shapeIntoMongooseObjectId(mb_id);
       const page = inquery["page"] ? inquery["page"] * 1 : 1;
       const limit = inquery["limit"] ? inquery["limit"] * 1 : 5;
@@ -54,9 +55,51 @@ class Community {
             },
           },
           { $unwind: "$member_data" },
+          //todo: check auth member liked the chosen target
         ])
         .exec();
       assert.ok(result, Definer.article_err2);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getArticlesData(member, inquery) {
+    try {
+      const auth_mb_id = shapeIntoMongooseObjectId(member?._id);
+      let matches =
+        inquery.bo_id === "all"
+          ? { bo_id: { $in: board_id_enum_list }, art_status: "active" }
+          : { bo_id: inquery.bo_id, art_status: "active" };
+      inquery.limit *= 1;
+      inquery.page *= 1;
+
+      const sort = inquery.order
+        ? { [`${inquery.order}`]: -1 }
+        : { createdAt: -1 };
+
+      const result = await this.boArticleModel
+        .aggregate([
+          { $match: matches },
+          { $sort: sort },
+          { $skip: (inquery.page - 1) * inquery.limit },
+          { $limit: inquery.limit },
+          {
+            $lookup: {
+              from: "members",
+              localField: "mb_id",
+              foreignField: "_id",
+              as: "member_data",
+            },
+          },
+          { $unwind: "$member_data" },
+          //todo: check auth member liked the chosen target
+        ])
+        .exec();
+
+      assert.ok(result, Definer.article_err3);
+
       return result;
     } catch (err) {
       throw err;
